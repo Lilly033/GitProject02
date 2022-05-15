6202c12e59bb554ce95c04ac35892ae8c5eadebf
Lilly033 <ljiljanajevtinijevic@gmail.com>
2022-05-15 10:53:16 -0700

    1st commit

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,12 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Something 1");
+        System.out.println("Something 2");
+        System.out.println("Something 3");
+    }
+}
