a149e70a04ce6056ae423275969cf4320f39ccc3
Lilly033 <ljiljanajevtinijevic@gmail.com>
2022-05-15 12:29:04 -0700

    the updated commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+//TEAM LEAD PROJECT
         System.out.println("Something 1");
         System.out.println("Something 2");
         System.out.println("Something 3");
@@ -13,5 +13,6 @@ public class Main {
 
         System.out.println("Something 6...6 corect code");
         System.out.println("Something 7");
+        System.out.println("hello my friend");
     }
 }
