1535eb5b3d63ab9fdb8847fc3aa490ceec8c10dd
Lilly033 <ljiljanajevtinijevic@gmail.com>
2022-05-15 11:01:43 -0700

    2nd commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -8,5 +8,7 @@ public class Main {
         System.out.println("Something 1");
         System.out.println("Something 2");
         System.out.println("Something 3");
+        System.out.println("Something 4");
+        System.out.println("Something 5");
     }
 }
